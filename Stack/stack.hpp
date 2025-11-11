83e8b9ac63201d3f7a8d183f862ad0472ef82c47
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2025-11-11 15:16:33 +0700

    stack file

--- Stack/stack.hpp
+++ Stack/stack.hpp
@@ -1,1 +1,24 @@
 
+template <typename T>
+
+class Stack {
+    private:
+
+
+    public:
+        void push(item){
+
+        }
+
+        dtype pop() {
+
+        }
+
+        dtype peek() {
+
+        }
+
+        bool empty() {
+            
+        }
+};
