0830acc2277fe1f4b78be1e5270104e5303d6e4d
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2025-11-12 16:44:29 +0700

    main and stack

--- Stack/stack.hpp
+++ Stack/stack.hpp
@@ -1,24 +1,46 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <iostream>
+using namespace std;
 
 template <typename T>
-
 class Stack {
-    private:
+private:
+    int MAX;   
+    T* data;   
+    int lastElement;
 
+public:
+    Stack(int maxSize) {
+        MAX = maxSize;
+        data = new T[MAX];
+        lastElement = -1;
+    }
 
-    public:
-        void push(item){
-
+    void push(T item) {
+        if (lastElement == MAX - 1) {
+            cout << "Stack overflow!" << endl;
+            return;
         }
+        data[++lastElement] = item;
+    }
 
-        dtype pop() {
-
+    T pop() {
+        if (empty()) {
+            cout << "Stack underflow!" << endl;
+            return T();
         }
+        return data[lastElement--];
+    }
 
-        dtype peek() {
+    T peek() {
+        if (empty()) return T();
+        return data[lastElement];
+    }
 
-        }
-
-        bool empty() {
-            
-        }
+    bool empty() {
+        return lastElement == -1;
+    }
 };
+#endif
